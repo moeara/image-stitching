d1cccc3506cbbef10c96c9c95bf53e2b329cc3e4
Mo-637 <moemen637@hotmail.com>
2021-01-03 11:32:30 +0200

    Added descriptive comments for clarity

--- demo/gui.py
+++ demo/gui.py
@@ -11,7 +11,7 @@ def traditional_stitcher(images):
     for image in images:
         keypoints, descriptor = sift.detectAndCompute(image, None)
         keypoint_image = image[:]
-        keypoint_images.append(cv2.drawKeypoints(images[0], keypoints, keypoint_image))
+        keypoint_images.append(cv2.drawKeypoints(image, keypoints, keypoint_image))
     
     return keypoint_images
 
@@ -26,12 +26,16 @@ def crop_edges(panorama, bordersize=10, mean=0):
 
 
 def invariant_stitcher(images, clean_pano=False):
+
+    # Create a stitcher object
     stitcher = cv2.Stitcher_create()
 
     (ref, panorama) = stitcher.stitch(images)
 
+    # Check if the stitching was successful, identified based on the returned ref
     if ref == 0:
         if clean_pano:
+            # Perform boundary crop for a clean border result
             print('Cleaning...')
             return crop_edges(panorama)
         return panorama
@@ -40,10 +44,13 @@ def invariant_stitcher(images, clean_pano=False):
 
 
 def main():
+    # Create upload interface for selecting inmages to be uploaded  
     multiple_pngs = st.file_uploader("Upload your set of PNG/JPEG images...", type=([".png", ".jpeg"]), accept_multiple_files=True)
+    
     uploaded_images = []
 
     if multiple_pngs:
+        # Read and decode the uploaded images and save them to a list
         for file_png in multiple_pngs:
             file_bytes = np.asarray(bytearray(file_png.read()), dtype=np.uint8)
             image = cv2.imdecode(file_bytes, 1) # Read in as a 3-channel image for the stitch function
@@ -51,8 +58,10 @@ def main():
             
         print("No. of images uploaded", len(uploaded_images))
 
+        # Perform stitching using OpenCV's native stitching function
         panorama = invariant_stitcher(uploaded_images)
 
+        # Display the resulting panorama
         if panorama is not None:
             st.image(panorama)
         else:
