1c2ac1f9b953bec6be178d70e6df00be75c69090
Mo-637 <moemen637@hotmail.com>
2021-01-03 14:16:13 +0200

    Finalised function for cropping/cleaning the image edges.

--- demo/gui.py
+++ demo/gui.py
@@ -16,13 +16,29 @@ def traditional_stitcher(images):
     return keypoint_images
 
 
-def crop_edges(panorama, bordersize=10, mean=0):
+def crop_edges(panorama, pixel_bound=50, bordersize=10, mean=0):
+    '''
+    Finds the minimum bounding rectangle and returns a cropped, cleaned panorama image.
+    1. Convert the original panorama to a single channel image.
+    2. Threshold the gray panorama to seperate the edge pixels.
+    3. Find the edges in the image that have more than 90% valid pixels.
+    3. Slice the orginal image.
+    '''
+
+    # Pad the borders of the original panorama
     panorama = cv2.copyMakeBorder(panorama, top=bordersize, bottom=bordersize, left=bordersize, right=bordersize,
                                   borderType=cv2.BORDER_CONSTANT, value=[mean, mean, mean])
+    grayscale_panorama = cv2.cvtColor(panorama, cv2.COLOR_BGR2GRAY)
+    _, threshold_image = cv2.threshold(grayscale_panorama, 1, 255, cv2.THRESH_BINARY)
 
-    #TODO: Find the max bounding rectangle and return a cropped, cleaned panorama image.
+    # Find the edges which contain more than 90% non-black (i.e. valid) pixels
+    horizontal_edge = (np.count_nonzero(threshold_image, axis=1) > 0.9*threshold_image.shape[1]).nonzero()
+    vertical_edge = (np.count_nonzero(threshold_image, axis=0) > 0.9*threshold_image.shape[0]).nonzero()
+    
+    y1, y2 = horizontal_edge[0][0], horizontal_edge[0][-1]
+    x1, x2 = vertical_edge[0][0], vertical_edge[0][-1]
 
-    return panorama
+    return panorama[y1:y2, x1:x2]
 
 
 def invariant_stitcher(images, clean_pano=False):
@@ -59,7 +75,7 @@ def main():
         print("No. of images uploaded", len(uploaded_images))
 
         # Perform stitching using OpenCV's native stitching function
-        panorama = invariant_stitcher(uploaded_images)
+        panorama = invariant_stitcher(uploaded_images, clean_pano=True)
 
         # Display the resulting panorama
         if panorama is not None:
