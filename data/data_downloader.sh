24d143bcf506070df52d2bebb778084574e03f9f
Mo-637 <moemen637@hotmail.com>
2021-01-07 11:47:54 +0200

    Add initial data download script

---
+++ data/data_downloader.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget -O adobe_panoramas.tgz https://sourceforge.net/projects/adobedatasets.adobe/files/adobe_panoramas.tgz/download && \
+unzip -O adobe_panoramas.tgz && \
+rm adobe_panoramas.tgz
