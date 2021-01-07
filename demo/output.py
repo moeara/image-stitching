3691169a8972e8310d2bccb78e9b2cceaedd8e9d
Mo-637 <moemen637@hotmail.com>
2021-01-07 13:51:24 +0200

    Added display for uploaded images alongside the resulting panorama

--- demo/output.py
+++ demo/output.py
@@ -4,19 +4,25 @@ def visualise(panorama, uploaded_images, mode_selection, modes=['Simplified Outp
     if mode_selection == modes[1]:
             verbose_output(panorama, uploaded_images)
     else:
-        simplified_output(panorama)
+        simplified_output(panorama, uploaded_images)
 
 
 def verbose_output(panorama, uploaded_images):
     pass
 
 
-def simplified_output(panorama):
+def simplified_output(panorama, uploaded_images):
     # Display the resulting panorama
     if panorama is not None:
         st.sidebar.text('Stitching Successful!')
+
+        # Display resulting panorama
         st.subheader('Resulting Panorama')
         st.image(panorama, use_column_width=True)
+
+        # Display all the uploaded images
+        st.subheader('Uploaded Images (Displayed in the order of upload)')
+        st.image(uploaded_images, width=200)
     else:
         st.markdown("Insufficient data, please use more input images or ensure the images have overlapping features...")
         st.sidebar.text("Unable to stitch images...")
